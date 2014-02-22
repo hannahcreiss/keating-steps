22e68074dbf84d4b0051dda759a4bbe2d92e1c08
Hannah Reiss <hannahcreiss@gmail.com>
2014-02-22 11:56:28 -0500

    class presentation version
    not yet responsive, contact us in footer

--- js/project.js
+++ js/project.js
@@ -1,8 +1,8 @@
-
 $(document).ready(function() {
 	$(function(){
 
-        
+        $(".fancybox").fancybox();
+
  
          $(window).scroll(function(){
                   if( $(window).scrollTop() > 300) {
@@ -15,19 +15,20 @@ $(document).ready(function() {
                 }
                     });
 
-        $(".thumb").mouseover(function(){
-            $("#videoTitle h2").html($(this).attr("alt"));
-        });
+            $(".thumb").mouseenter(function(){
+                $("#videoTitle h2").html($(this).attr("alt"));
+            });
 
-        $(".thumb").mouseleave(function(){
-            $("#videoTitle h2").html(" ");
-        })
-
+            $(".thumb").mouseleave(function(){
+                $("#videoTitle h2").html($("#displayVideo iframe").attr("alt"));
+            });
+        
 
         $(".thumb").click(function(){
-        
+            
             var id= $(this).attr("id");
             var sub= id.substring(5);
+            var lastClicked= $(this).attr("alt");
 
 
     
@@ -35,18 +36,57 @@ $(document).ready(function() {
 
             $("#"+sub).css("display", "block");
 
+            $("videoTitle h2").html($(this).attr("alt"));
+
+
+            $(".thumb").mouseenter(function(){
+                $("#videoTitle h2").html($(this).attr("alt"));
+            });
+
+            $(".thumb").mouseleave(function(){
+                $("#videoTitle h2").html(lastClicked);
+            });
+       
+
         });
 
-        $("#right").click(function(){
-            $("#thumbnails").animate({"left": "-=30"}, 500);
-    });
+
+
+        $("#rightButton").click(function(){
+            if (($("#thumbnails").position().left <= 0) && ($("#thumbnails").position().left > -420)) {
+                $("#thumbnails").animate({"left": "-=108"}, 1000);
+            }   
+
+            if ($("#thumbnails").position().left >= -108) {
+                $("#leftButton").css("opacity", ".8");
+            } 
+
+            if ($("#thumbnails").position().left <= -324) {
+                $("#rightButton").css("opacity", ".2");
+            }
+
+        });
     
+         $("#leftButton").click(function(){
+            if (($("#thumbnails").position().left <= -108) && ($("#thumbnails").position().left > -433)) {
+                    $("#thumbnails").animate({"left": "+=108"}, 1000);
+              }
+
+            if ($("#thumbnails").position().left >= -108) {
+                $("#leftButton").css("opacity", ".2");
+            }
+
+            if ($("#thumbnails").position().left >= -432) {
+                $("#rightButton").css("opacity", ".8");
+            }
+         });
+
+
+         
+         
             
       
   });
 
-    $("#videos").click(function(){
-        $.scrollTo("38%", 2000);
-        });
 
 });
